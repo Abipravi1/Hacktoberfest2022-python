d2518ee8577ca825b7830dcd3bc21fd7a68d63fe
ALEN S ABRAHAM <alensabraham007@gmail.com>
2021-10-27 07:56:41 +0400

    commit message

--- program 2 updated.py
+++ program 2 updated.py
@@ -5,7 +5,7 @@
 # First import the calendar module  
 import calendar  
 # ask of month and year  
-yy = int(input("Enter year: "))  
-mm = int(input("Enter month: "))  
+yy = int(2021)  
+mm = int(10)  
 # display the calendar  
 print(calendar.month(yy,mm))
