bda77d51c03caa1b88b261a07adb727c053f43bb
Indrahas Reddy Vennapusa <19D070067@iitb.ac.in>
2022-10-03 01:46:15 +0530

    added code to download all xkcd comics

--- download_xkcd.py
+++ download_xkcd.py
@@ -5,7 +5,7 @@ import requests, os, bs4
 url = 'https://xkcd.com'               # starting url
 os.makedirs('xkcd', exist_ok=True)    # store comics in ./xkcd
 while not url.endswith('#'):
-    print('Downloading page %s...' % url)
+    print('Downloading page  %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
 
