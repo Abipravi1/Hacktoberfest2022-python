d49d0d89c24b977fefe9148b118c661bb40bbd98
SHIVA MANI <86513644+ShivaMani02@users.noreply.github.com>
2022-10-09 15:50:42 +0530

    Create string punctuation in Python.py

---
+++ Python Basic Programs/string punctuation in Python.py
@@ -0,0 +1,8 @@
+# import string library function
+import string
+	
+# Storing the sets of punctuation in variable result
+result = string.punctuation
+	
+# Printing the punctuation values
+print(result)
